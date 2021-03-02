dceeab016d290aaafbf25c5ccecef38417667446
askatanarbaev <askatanarbaevchik@gmail.com>
2021-03-02 16:06:52 +0600

    done: comment-form, post-delete,

--- insta/urls.py
+++ insta/urls.py
@@ -7,9 +7,13 @@ from django.urls import path
 urlpatterns = [
     path('', PostListView.as_view(), name='home'),
     path('my_profile/', my_profile, name='my_profile'),
-    path('create_profile', NewProfileView.as_view(), name='create_profile'),
-    path('my_profile/create_post/', post_create, name='create_post')
-
+    path('create_profile/', NewProfileView.as_view(), name='create_profile'),
+    path('my_profile/create_post/', post_create, name='create_post'),
+    path('my_profile/edit_profile/', edit_profile, name='edit_profile'),
+    path('<int:pk>/delete/', post_delete, name='post-delete'),
+    path('<int:pk>/', post_detail, name='post-detail'),
+    path('<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('search_posts/', search_posts, name='search_posts'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- insta/views.py
+++ insta/views.py
@@ -57,4 +57,92 @@ def post_create(request):
     return render(request, 'post_create.html', locals())
 
 
+# delete and update
 
+# def edit_profile(request):
+#     post = Profile.objects.all()
+#     if request == 'POST':
+#         form = CreateProfileForm(request.POST, instance=post)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.created_by = request.user
+#             post.save()
+#             return redirect('home')
+#     else:
+#         form = CreateProfileForm(instance=post)
+#     return render(request, 'edit_profile.html', locals())
+
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('posts_list')
+    return render(request, 'blog/post/post_delete.html', {'post': post})
+
+
+
+@login_required
+def edit_profile(request):
+	if request.method == 'POST':
+		p_form = CreateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+		if p_form.is_valid():
+			p_form.save()
+			return redirect('my_profile')
+	else:
+		p_form = CreateProfileForm(instance=request.user.profile)
+	return render(request, 'edit_profile.html', locals())
+
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	model = Post
+	fields = ['description', 'image']
+	template_name = 'update_post.html'
+
+	def form_valid(self, form):
+		form.instance.username = self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.created_by:
+			return True
+		return False
+
+
+@login_required
+def post_delete(request, pk):
+	post = Post.objects.get(pk=pk)
+	if request.user == post.created_by:
+		Post.objects.get(pk=pk).delete()
+	return redirect('home')
+
+
+
+
+# work with comments
+
+@login_required
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	user = request.user
+	if request.method == 'POST':
+		form = NewCommentForm(request.POST)
+		if form.is_valid():
+			data = form.save(commit=False)
+			data.post = post
+			data.username = user
+			data.save()
+			return redirect('post-detail', pk=pk)
+	else:
+		form = NewCommentForm()
+	return render(request, 'post_detail.html', locals())
+
+# search
+
+@login_required
+def search_posts(request):
+	query = request.GET.get('p')
+	object_list = Post.objects.filter(description__icontains=query)
+	return render(request, "search_posts.html", locals())
